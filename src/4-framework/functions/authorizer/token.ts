9ccbffacd6d26729e8e109da653f16bf9f313966
Marcos Dornellas <mvdornellas@stefanini.com>
2021-03-16 21:04:40 -0300

    authorizer token

--- src/4-framework/functions/authorizer/token.ts
+++ src/4-framework/functions/authorizer/token.ts
@@ -1,18 +1,21 @@
 import 'reflect-metadata'
-import { APIGatewayEvent } from 'aws-lambda'
 import { AuthorizerController } from '#adapter/controllers/authorizerController'
 import { TokenAuthorizerInput } from '#adapter/serializers/authorizer/tokenInput'
 import { Container } from 'typedi/Container'
 
-export const handler = async (event: APIGatewayEvent, _context: any, callback: any) => {
-  let token = event.queryStringParameters && event.queryStringParameters.token ? event.queryStringParameters.token : undefined
+export const handler = async (event: any, _context: any, callback: any) => {
+  const token = event.authorizationToken
+  console.log(`[I] TOKEN`, token)
   const input: TokenAuthorizerInput = new TokenAuthorizerInput({
     ...event,
-    token: token
+    token
   })
   console.log('[I] TOKEN AUTHORIZER INPUT', input)
   const operator = Container.get(AuthorizerController)
   const output = await operator.token(input)
-  console.log('[I] TOKEN AUTHORIZER OUTPUT', output)
+  console.log('[I] TOKEN AUTHORIZER OUTPUT', JSON.stringify(output))
+  if (output.authorizer.policyDocument.Statement[0].Effect === 'Deny') {
+    return callback('Unauthorized')
+  }
   return callback(null, output.authorizer)
 }
